4f159e4a4dc64a60ad565c81e5f491bd0247f98c
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-06-19 19:13:48 -0500

    add square

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import './App.css'
 
-const TURNS = { 
-  X: 'x', 
-  O: 'o', 
-} 
+const TURNS = {
+  X: 'x',
+  O: 'o',
+}
+
+const board = Array(9).fill(null)
+
+const Square = ({ children, updateBoard, index }) => {
+  <div className='square' key={index}>
+    { /* Render the turn here */}
+    { /* Example: <span>{TURNS.X}</span> */}
+  </div>
+}
 
 function App() {
 
-  return <h1>Tic Tac Toe</h1>
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              
+            )
+          })
+        }
+      </section>
+    </main>
+  )
 
 }
 
