7387e498fbfb95f684af59d7897f445f2857df1a
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-06-19 22:02:06 -0500

    add square

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,27 @@ const TURNS = {
   O: 'o',
 }
 
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard()
+  }
+
   return (
-    <div className='square' >
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
 
 function App() {
-  const [board, setBoard] = useState([Array(9).fill(null)])
+  const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = () => {
+
+  }
 
   return (
     <main className="board">
@@ -26,12 +36,25 @@ function App() {
         {
           board.map((_, index) => {
             return (
-              <Square key={index} index={index} >
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
                 {board[index]}
               </Square>
             )
           })
         }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
   )
